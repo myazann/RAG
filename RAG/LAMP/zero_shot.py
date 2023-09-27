1ebd08ab6ab2128ce520609bf61c479b320a9863
myazann <mertyazan2867@gmail.com>
2023-09-27 14:20:48 +0200

    added more models

--- RAG/LAMP/zero_shot.py
+++ RAG/LAMP/zero_shot.py
@@ -1,13 +1,11 @@
-#module load GitPython
-#source /data1/yazanm/chatbots/bin/activate
-#module load ALICE/AMD
-#module load GitPython/3.1.14-GCCcore-10.2.0 
-
+import sys
 import os
 import time
+import subprocess
 
 from langchain import LLMChain, PromptTemplate
 from evaluate import load
+import torch 
 
 from RAG.prompter import Prompter
 from RAG.utils import get_args
@@ -19,14 +17,14 @@ dataset_num = args.lamp_dataset_num
 
 data, gts = FileLoader.get_lamp_dataset(dataset_num)
 prompter = Prompter()
-# model_name="LLAMA2-7B",
-chatbot = choose_bot(gen_params={"max_new_tokens": 256}, q_bits=5)
+
+chatbot = choose_bot(model_name="LLAMA2-70B-GGUF", gen_params={"max_new_tokens": 256}, q_bits=5)
+print(subprocess.run("nvidia-smi"))
+
+sys.stdout.flush()
 lamp_prompt = prompter.merge_with_template(chatbot, f"lamp_{dataset_num}")
 
-if chatbot.q_bit is None:
-  test_name = f"QA_{chatbot.name}_{time.time()}"
-else:
-  test_name = f"QA_{chatbot.name}_{chatbot.q_bit}-bit_{time.time()}"
+test_name = f"LAMP_{chatbot.name}_{chatbot.q_bit}-bit_{time.time()}"
 os.environ["LANGCHAIN_PROJECT"] = test_name
 
 llm_chain = LLMChain(llm=chatbot.pipe, prompt=PromptTemplate.from_template(lamp_prompt))
@@ -38,7 +36,8 @@ if dataset_num == "2":
             break
         article_idx = q["input"].rfind("article")
         article = q["input"][(article_idx+len("article: ")):].strip()
-        res = llm_chain.predict(article=article).strip()
+        final_prompt = lamp_prompt.format(article=article)
+        res = chatbot.pipe(final_prompt)
         all_res.append(res)
         print(f"Article: {article}")
         print(f"Ground Truth: {gts[i]['output']}")
@@ -49,17 +48,20 @@ if dataset_num == "2":
 
 elif dataset_num == "5":
     for i, q in enumerate(data):
-        if i == 20:
+        if i == 10:
             break
         print(f"Sample {i}:\n")
-        abstract_idx = q["input"].find(":")+1
+        abstract_idx = q["input"].find(":") + 1
         abstract = q["input"][abstract_idx:].strip()
-        res = llm_chain.predict(abstract=abstract).strip()
+        print(subprocess.run("gpustat"))
+        final_prompt = lamp_prompt.format(abstract=abstract)
+        res = chatbot.pipe(final_prompt)
         gt = gts[i]['output']
         print(f"Abstract: \n{abstract}")
         print(f"Ground Truth: \n{gt}")
         print(f"Pred: \n{res}")
 
+        """
         bertscore = load("bertscore")
         bertscore_res = bertscore.compute(predictions=[res], references=[gt], lang="en")
         print(f"Bertscore: {bertscore_res}")
@@ -71,4 +73,6 @@ elif dataset_num == "5":
         bleu = load("bleu")
         bleu_results = bleu.compute(predictions=[res], references=[gt])
         print(f"BLEU: {bleu_results}")
-        print()
+        """
+        torch.cuda.empty_cache()
+        sys.stdout.flush()
